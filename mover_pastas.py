cc136ed13afb8954bc7376edd6686c39a39f4b96
Ricardo <ricardo_cas4@hotmail.com>
2020-08-21 01:17:38 -0300

    adicionado um metodo para testar a movimentação de arquivos

--- mover_pastas.py
+++ mover_pastas.py
@@ -3,36 +3,48 @@ import shutil # biblioteca responsável para realizar a movimentação de arquiv
 from pathlib import Path
 import json # biblioteca usada para ler arquivo com os nomes dos diretorios que as pastas serão criadas
 
-def boas_vindas():
-    print('Hello World')
-
-def cria_pasta():
-    nome_pasta = input('Qual o nome da Pasta?: ')
+def testa_move_Pasta():
+    nome_pasta = input('Qual o nome da Pasta você deseja mover?: ')
 
     if not os.path.exists(nome_pasta):
             print("--------------------------------")
-            os.makedirs(nome_pasta)
-            print(f'A pasta "{nome_pasta}" foi criada!')
-            print("--------------------------------")
-            os.chdir('.') # muda o diretório para a pasta criada
+            print(f'A Pasta "{nome_pasta}" não existe!')
+            print("################################")
     else:
-            print("--------------------------------")
-            print(f'Pasta "{nome_pasta}" já foi criada')
-            print("--------------------------------")
-    
-def move_Pasta():
-    print('Logo vou mover algo')
+        # origem = os.listdir('.')
+        # print(origem)
+        for item in os.walk(nome_pasta):
+            print(item)
+            print(os.getcwd())
+            # os.chdir('.') # muda o diretório para a pasta criada
+        # os.chdir(origem) # muda o diretório para a pasta criada
+        # print(os.chdir(origem)) # muda o diretório para a pasta criada
 
+def move_Pasta():
+    nome_pasta = input('Qual o nome da Pasta você deseja mover?: ')
+
+    if not os.path.exists(nome_pasta):
+            print("--------------------------------")
+            print(f'A Pasta "{nome_pasta}" não existe!')
+            print("################################")
+    else:        
+        print("--------------------------------")
+        print(f'A Pasta "{nome_pasta}" foi movida!')
+        print("################################")
 
 def exibe_caminho_atual():
     print("O Caminho da pasta é: ")
     print(os.getcwd())
+    
 
 
 def inicia_programa():
-    boas_vindas()
     print("--------------------------------")
-    exibe_caminho_atual()
+    testa_move_Pasta()
     print("--------------------------------")
-    cria_pasta()
-    print("--------------------------------")
+
+
+
+if __name__ == "__main__":
+    inicia_programa()
+    
